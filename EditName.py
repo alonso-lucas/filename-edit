d78624220271dc64589e4baf525be35f183b2dd5
Lucas Alonso <lucasalonso598@gmail.com>
2022-04-19 03:08:04 -0300

    Comments were now added

--- EditName.py
+++ EditName.py
@@ -1,8 +1,12 @@
 #Script to remove "y2mate" from the begining of all filenames in Music folder
 import os
 
+#Change current working directory
 os.chdir(r'C:\Users\lucas\Desktop\Music')
 
+#Iterate over directory
 for file in os.listdir():
+    #Split filename by '-', creating a list with all parts
     nombreSeparado = (file.split('-'))
+    #Rename file as the last item on the list
     os.rename(file, nombreSeparado[-1])
